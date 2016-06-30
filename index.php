75ad59d2fa741c4eccd3a8eac229d9a63d1da9f6
unknown <swethasree582@gmail.com>
2016-06-30 16:33:32 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<head>
+<title>Midterm-15971</title>
+<style type="text/css">
+}
+#tb{
+	
+	text-align: center;
+}
+table{
+	border-collapse: collapse;
+}
+table,th,td{
+	border: 1px solid black;
+	
+}
+th.ci{
+	width: 20%;
+}
+</style>
+</head>
+<body>
+<table cellpadding="20">
+<tr>
+<th id="tb"colspan="3" rowspan="1" height="100" width="300"><b>      X      </th>
+</tr>
+<tr>
+<th id="tb" colspan="1" rowspan="1" height="100" width="100"><b>Y</th>
+<th id="tb"	 colspan="2" rowspan="1" height="100" width="200"><b>Z</th>
+</tr>
+<tr>
+	<th id="tb" colspan="3" rowspan="1" height="100" width="300"><b>M</th>
+</tr>
+</table>
+</body>
